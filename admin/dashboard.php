887d1d6fd3c04739c8f731770ff171b968d4b610
ExoticBaconDev <123257173+ExoticBaconDev@users.noreply.github.com>
2025-05-13 03:24:14 +0800

    Update dashboard.php

---
+++ admin/dashboard.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Dashboard</title>
+</head>
+<body>
+    <h1>Admin Dashboard</h1>
+    <ul>
+        <li><a href="manageusers.php">Manage Users</a></li>
+    </ul>
+</body>
+</html>
