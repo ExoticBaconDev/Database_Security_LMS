cb22e3ec3f302e13cdcd2a23077711739ab3c708
ExoticBaconDev <123257173+ExoticBaconDev@users.noreply.github.com>
2025-05-14 03:55:01 +0800

    Update dashboard.php

--- librarian/dashboard.php
+++ librarian/dashboard.php
@@ -9,6 +9,7 @@
     <h1>Librarian Dashboard</h1>
     <ul>
         <li><a href="addbook.php">Add Book</a></li>
+        <li><a href="addcategories.php">Add Categories</a></li>
         <li><a href="managebooks.php">Manage Books</a></li>
     </ul>
     <a href="../login.php"><button type="button">Logout</button></a>
