4dc10d1b9867deb456dc132c10c534e5b90f473e
ExoticBaconDev <123257173+ExoticBaconDev@users.noreply.github.com>
2025-05-18 18:54:54 +0800

    Create backup.php

---
+++ admin/backup.php
@@ -0,0 +1,12 @@
+<?php
+// Only allow admin to access backup
+session_start();
+if (!isset($_SESSION['user_name']) || $_SESSION['user_role'] !== 'Admin') {
+    die("Access denied.");
+}
+
+$backupFile = "librarydb_backup_" . date("Y-m-d_H-i-s") . ".bak";
+$cmd = "sqlcmd -S localhost -U php_user -P Pa$$w0rd -Q "BACKUP DATABASE [LibraryDB] TO DISK='C:\\xampp\\htdocs\\Database_Security_LMS-main\\backups\\$backupFile'"";
+system($cmd);
+echo "<p>Backup created: $backupFile</p>";
+?>
