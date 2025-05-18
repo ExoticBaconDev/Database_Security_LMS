cf04922aea7406c916ca2ac6e45cfb80c98d9d5f
ExoticBaconDev <123257173+ExoticBaconDev@users.noreply.github.com>
2025-05-18 19:30:55 +0800

    Update backup.php

--- admin/backup.php
+++ admin/backup.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['user_name']) || $_SESSION['user_role'] !== 'Admin') {
 }
 
 $backupFile = "librarydb_backup_" . date("Y-m-d_H-i-s") . ".bak";
-$cmd = "sqlcmd -S localhost -U php_user -P Pa$$w0rd -Q "BACKUP DATABASE [LibraryDB] TO DISK='C:\\xampp\\htdocs\\Database_Security_LMS-main\\backups\\$backupFile'"";
-system($cmd);
-echo "<p>Backup created: $backupFile</p>";
+$cmd = "sqlcmd -S localhost -U php_user -P Pa\$\$w0rd -Q \"BACKUP DATABASE [LibraryDB] TO DISK='C:\\xampp\\htdocs\\Database_Security_LMS-main\\backups\\$backupFile'\"";
+$output = shell_exec($cmd);
+echo "<pre>$output</pre>";
 ?>
